e1dc8511943a1b572e08ae9d0588e508036ff9b1
gabi-gabu1 <97314479+gabi-gabu1@users.noreply.github.com>
2022-04-20 08:16:38 +0200

    import React and axios

---
+++ src/Weather.js
@@ -0,0 +1,14 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+  function getTemperature(response) {
+    return alert(
+      `It is ${Math.round(response.data.main.temp)}°C in ${props.city}`
+    );
+  }
+  let apiKey = "a592e749eb4eda83dffcd2b9176f3c7f";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+  axios.get(url).then(getTemperature);
+}
