c019056c95afc02cdb3567051e9d2a42203c0e8f
gabi-gabu1 <97314479+gabi-gabu1@users.noreply.github.com>
2022-04-21 11:33:00 +0200

    Added Weather forecast features

--- src/App.js
+++ src/App.js
@@ -1,16 +1,144 @@
 import "./App.css";
-import Weather from "./Weather";
 
-function App() {
+export default function Weather() {
+  let weatherData = {
+    day: "Friday",
+    humidity: 80,
+    wind: 5,
+    temperature: 20,
+    city: "Zagreb",
+    description: "Clear sky",
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World!!!!</h1>
-        <Weather city="Brno" />
-        <Weather city="Singapore" />
-      </header>
+    <div className="container">
+      <div className="container-wrapper">
+        <div className="row search">
+          <div className="col-6">
+            <form />
+            <input
+              className="form-control mb-4 city-input"
+              type="text"
+              placeholder="Enter a city"
+              autocomplete="off"
+              autofocus
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="search-button btn btn-primary w-100"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Current"
+              className="search-button btn btn-success w-100"
+            />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-5 prague">
+            <h1 className="city-prague">{weatherData.city}</h1>
+
+            <ul>
+              <li className="date">{weatherData.day} 16:22</li>
+
+              <li>
+                <span>{weatherData.description}</span>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                Humidity: <span> {weatherData.humidity} </span>%
+              </li>
+              <li>
+                Wind: <span>{weatherData.wind}</span>km/hr
+              </li>
+            </ul>
+          </div>
+
+          <div className="col-3 weather-icon">
+            <li>
+              <img
+                src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-1024.png"
+                width="80px"
+                alt="icon"
+                className="icon"
+              />
+            </li>
+          </div>
+          <div className="col-4 temperature-container">
+            <span className="temperature-prague">
+              {weatherData.temperature}
+            </span>
+            <span className="units">°C</span>
+          </div>
+        </div>
+
+        <div className="forecast-days mt-4">
+          <div className="row">
+            <div className="col">
+              <p>Fri</p>
+              <img
+                src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-1024.png"
+                width="50px"
+                alt="icon"
+                className="icon"
+              />
+            </div>
+            <div className="col">
+              <p>Sat</p>
+              <img
+                src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-1024.png"
+                width="50px"
+                alt="icon"
+                className="icon"
+              />
+            </div>
+            <div className="col">
+              <p>Sun</p>
+              <img
+                src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-1024.png"
+                width="50px"
+                alt="icon"
+                className="icon"
+              />
+            </div>
+            <div className="col">
+              <p>Mon</p>
+              <img
+                src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-1024.png"
+                width="50px"
+                alt="icon"
+                className="icon"
+              />
+            </div>
+            <div className="col">
+              <p>Tue</p>
+              <img
+                src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-1024.png"
+                width="50px"
+                alt="icon"
+                className="icon"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <p className="open-source">
+        <a
+          href="https://github.com/gabi-gabu1/weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>
+        by Gabriela Dontamsetti
+      </p>
     </div>
   );
 }
-
-export default App;
--- src/Weather.js
+++ src/Weather.js
@@ -1,13 +1,87 @@
-import axios from "axios";
+export default function Weather() {
+  let weatherData = {
+    day: "Friday",
+    humidity: 80,
+    wind: 5,
+    temperature: 20,
+    city: "Zagreb",
+    description: "Clear sky",
+  };
+  return (
+    <div className="container">
+      <div className="container-wrapper">
+        <div className="row search">
+          <div className="col-9">
+            <form />
+            <input
+              className="form-control mb-4 city-input"
+              type="text"
+              placeholder="Enter a city"
+              autocomplete="off"
+              autofocus
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="search-button btn btn-primary w-100"
+            />
+          </div>
+        </div>
 
-export default function Weather(props) {
-  function getTemperature(response) {
-    return alert(
-      `It is ${Math.round(response.data.main.temp)}°C in ${props.city}`
-    );
-  }
-  let apiKey = "a592e749eb4eda83dffcd2b9176f3c7f";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+        <div className="row">
+          <div className="col-5 prague">
+            <h1 className="city-prague">{weatherData.city}</h1>
 
-  axios.get(url).then(getTemperature);
+            <ul>
+              <li className="date">{weatherData.day} 16:22</li>
+
+              <li>
+                <span>{weatherData.description}</span>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                Humidity: <span> {weatherData.humidity} </span>%
+              </li>
+              <li>
+                Wind: <span>{weatherData.wind}</span>km/hr
+              </li>
+            </ul>
+          </div>
+
+          <div className="col-3 weather-icon">
+            <li>
+              <img
+                src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-1024.png"
+                width="80px"
+                alt="icon"
+                className="icon"
+              />
+            </li>
+          </div>
+          <div className="col-4 temperature-container">
+            <span className="temperature-prague">
+              {weatherData.temperature}
+            </span>
+            <span className="units">°C</span>
+          </div>
+        </div>
+
+        <div className="forecast-days"></div>
+      </div>
+
+      <p className="open-source">
+        <a
+          href="https://github.com/gabi-gabu1/weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>
+        by Gabriela Dontamsetti
+      </p>
+    </div>
+  );
 }
